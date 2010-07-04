431ca4ec74a09a962578adb50a293c0ece4e714e
ben@benatkin.com <ben@benatkin.com>
2010-07-04 05:00:31 -0600

    added project file, that can run compojure in a repl

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject chardin "0.0.1-SNAPSHOT"
+  :description "Stretching the concept of a wiki"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+		 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [compojure "0.4.0"]
+                 [ring/ring-jetty-adapter "0.2.3"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
