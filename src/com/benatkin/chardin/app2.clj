cf7c4248525fcf38e1ee464523ea2d83b7b71616
ben@benatkin.com <ben@benatkin.com>
2010-07-10 18:38:14 -0600

    added simple routing

--- src/com/benatkin/chardin/app2.clj
+++ src/com/benatkin/chardin/app2.clj
@@ -7,6 +7,23 @@
    :headers {"Content-Type" "text/html"}
    :body    "<p>Resource not found.</p>"})
 
+(def *routes*
+  '[(#"^$"     {:page "home"})
+    (#"^(.*)$" {:page 1})])
+
+(defn route [path]
+  (defn route-groups [a-route]
+    (re-matches (first a-route) path))
+  (defn route-data [a-route]
+    (let [groups (route-groups a-route)
+          data (second a-route)]
+      (defn get-group [value] (if (number? value) (groups value) value))
+      (if groups
+          (zipmap (keys data)
+                  (map get-group (vals data))))))
+  (let [matching-route (first (filter route-groups *routes*))]
+    (route-data matching-route)))
+
 (defn start-app []
   (run-jetty web-app {:port 8080}))
 
--- test/com/benatkin/chardin/app2_test.clj
+++ test/com/benatkin/chardin/app2_test.clj
@@ -2,12 +2,16 @@
   (use [com.benatkin.chardin.app2] :reload-all)
   (use [clojure.test]))
 
+(deftest test-route
+  (is (= (route "bad-80s-music") {:page "bad-80s-music"}))
+  (is (= (route "") {:page "home"})))
+
 (defn req [path]
   {:request-method :get
-   :uri (str "http://localhost:8080" path)})
+   :uri (str "http://localhost:8080/" path)})
 
 (deftest test-app
-  (let [response (web-app (req "/bad-80s-music"))]
+  (let [response (web-app (req "bad-80s-music"))]
     (is (= (:status response) 404))
-    (is (= (:headers "Content-Type") "text/json"))
+    (is (= ((:headers response) "Content-Type") "text/html"))
     (is (re-find #"(?i)not found" (:body response)))))
